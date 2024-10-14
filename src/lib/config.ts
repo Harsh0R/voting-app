06a8ba5094cf326d532d6c2b256e1db2551a9fd4
Harsh <harshradadiya9999@gmail.com>
2024-10-14 12:58:46 +0530

    update

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,19 +1,32 @@
 'use client';
-import { http, createStorage, cookieStorage } from 'wagmi'
-import { sepolia, bscTestnet, blastSepolia, polygonAmoy } from 'wagmi/chains'
-import { Chain, getDefaultConfig } from '@rainbow-me/rainbowkit'
+import { http, createStorage, cookieStorage } from 'wagmi';
+import { sepolia, bscTestnet, blastSepolia, polygonAmoy } from 'wagmi/chains';
+import { Chain, getDefaultConfig } from '@rainbow-me/rainbowkit';
 
+// Define the project ID
 const projectId = 'YOUR_PROJECT_ID';
 
-const supportedChains: Chain[] = [polygonAmoy ,sepolia, bscTestnet, blastSepolia];
+// Use the Chain type from @rainbow-me/rainbowkit directly
+const supportedChains: Chain[] = [
+   polygonAmoy,
+   sepolia,
+   bscTestnet,
+   blastSepolia,
+];
 
 export const config = getDefaultConfig({
    appName: 'WalletConnection',
    projectId,
-   chains: supportedChains,
+   chains: [polygonAmoy,
+      sepolia,
+      bscTestnet,
+      blastSepolia,],
    ssr: true,
    storage: createStorage({
-    storage: cookieStorage,
+      storage: cookieStorage,
    }),
-  transports: supportedChains.reduce((obj, chain) => ({ ...obj, [chain.id]: http() }), {})
- });
+   transports: supportedChains.reduce((obj, chain) => ({
+      ...obj,
+      [chain.id]: http(),
+   }), {} as Record<number, ReturnType<typeof http>>),
+});
